1d74dd695f780c7f370667cb3850279bbc971144
chenliu1993 <cl2037829916@gmail.com>
2025-06-02 04:53:52 +0000

    t

--- build.sh
+++ build.sh
@@ -19,12 +19,14 @@ cd bpftool && git submodule update --init
 cd src && sudo make install && sudo install ./bpftool /usr/local/sbin/bpftool
 
 cd ${srcPath}
-echo ${srcPath}
+ls ${srcPath}
+ls ${srcPath}/driver
+
 rm -rf skeleton-build build
 
-cmake -B skeleton-build -S . -DUSE_BUNDLED_DEPS=ON -DCREATE_TEST_TARGETS=Off -DFALCO_VERSION=${VERSION} -DFALCOSECURITY_LIBS_SOURCE_DIR="$(pwd)/libs" -DDRIVER_SOURCE_DIR="$(pwd)/libs/driver"
+cmake -B skeleton-build -S . -DUSE_BUNDLED_DEPS=ON -DCREATE_TEST_TARGETS=Off -DFALCO_VERSION=${VERSION} -DFALCOSECURITY_LIBS_SOURCE_DIR="${srcPath}/libs" -DDRIVER_SOURCE_DIR="${srcPath}/libs/driver"
 cmake --build skeleton-build --target ProbeSkeleton -j12
 
-cmake -B build -S . -DCMAKE_BUILD_TYPE=RelWithDebInfo -DUSE_BUNDLED_DEPS=On -DFALCO_ETC_DIR=/etc/falco -DMODERN_BPF_SKEL_DIR=$(pwd)/skeleton-build/skel_dir -DBUILD_DRIVER=Off -DBUILD_BPF=Off -DUSE_JEMALLOC=ON -DFALCO_VERSION=${VERSION} -DFALCOSECURITY_LIBS_SOURCE_DIR="$(pwd)/libs" -DDRIVER_SOURCE_DIR="$(pwd)/libs/driver"
+cmake -B build -S . -DCMAKE_BUILD_TYPE=RelWithDebInfo -DUSE_BUNDLED_DEPS=On -DFALCO_ETC_DIR=/etc/falco -DMODERN_BPF_SKEL_DIR=${srcPath}/skeleton-build/skel_dir -DBUILD_DRIVER=Off -DBUILD_BPF=Off -DUSE_JEMALLOC=ON -DFALCO_VERSION=${VERSION} -DFALCOSECURITY_LIBS_SOURCE_DIR="${srcPath}/libs" -DDRIVER_SOURCE_DIR="${srcPath}/libs/driver"
 cmake --build build --target falco -j8
 cmake --build build --target package
